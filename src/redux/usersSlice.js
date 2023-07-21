e6546f33e714bd87aa46c38d27cd44f6f20cc78c
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-07-21 17:07:31 +0600

    Add usersSlice

---
+++ src/redux/usersSlice.js
@@ -0,0 +1,41 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const URL = 'https://randomuser.me/api/'
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async (thunk) => {
+  try {
+    
+    const response = await axios(URL);
+    return response.data;
+  } catch (error) {
+    return thunk.rejectWithValue(error.message);
+  }
+});
+
+
+const usersSlice = createSlice({
+  name: 'users',
+  initialState: {
+    users: [],
+    isLoading: false,
+    error: undefined,
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchUsers.pending, (state) => {
+      state.isLoading = true;
+      state.error = undefined;
+    });
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.users = action.payload;
+    });
+    builder.addCase(fetchUsers.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    });
+  },
+});
+
+
+export const { actions } = usersSlice;
+export default usersSlice.reducer;
