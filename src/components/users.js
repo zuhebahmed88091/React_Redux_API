76cccca7f7090322247c2158b5c94b506bdb84e2
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-07-21 17:09:03 +0600

    Add users and user in components

---
+++ src/components/users.js
@@ -0,0 +1,32 @@
+import { useSelector,useDispatch } from "react-redux";
+import React,{useEffect} from "react";
+import { fetchUsers } from "../redux/users/usersSlice";
+import User from "./user";
+
+const UsersContainer = () => {
+  const { users, isLoading, error } = useSelector((state) => state.users);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  return (
+    <ul>
+      {users.map((user) => (
+        <User key={user.id} user={user} />
+      ))}
+    </ul>
+  );
+};
+
+
+export default UsersContainer;
